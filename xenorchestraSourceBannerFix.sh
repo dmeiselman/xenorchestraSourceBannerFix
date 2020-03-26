7f1c1c4aa79b9d8886ab84d3fe3bcb6bf8de428a
megabert <faxmail09@gmx.net>
2020-03-26 18:44:32 +0100

    New version of script -  improved robustness

--- xenorchestraSourceBannerFix.sh
+++ xenorchestraSourceBannerFix.sh
@@ -1,25 +1,68 @@
 #!/bin/bash
 
-##FOXIEHAZMAT##
-##Version: 1.0
-##XOA Compatiblility: v5.34.0
+#
+#       This script removes the open source disclaimer
+#       of Xen Orchestra 5
+#
+#       how to run:
+#
+#       place it in the directory where you ran the
+#       git clone ... xen-orchestra
+#
 
-####### SUDO IS REQUIRED #######
+myinit() {
 
-#stop xo-server
-systemctl stop xo-server
+        if ! [ -d "xen-orchestra" ]; then
 
-#find xo-web install path
-cd $(find / -wholename */xen-orchestra/packages/xo-web)
+                echo
+                echo "You are not in the parent directory of the xen-orchestra checkout"
+                echo "this script must be run from there"
+                echo
+                exit 1
 
-#backup index file
-cp src/xo-app/index.js src/xo-app/index.js.bak
+        fi
 
-#remove no-no lines
-sed -i '225,235 d' src/xo-app/index.js
+        if ! [ -r "$file" ]; then
 
-#recompile xo-web
-yarn build
+                echo 
+                echo "target file '$file' can not be found, aborting"
+                echo
+                exit 1
 
-#start xo-server
-systemctl start xo-server
+        fi
+}
+
+# 
+# remove the following code fragment
+#
+#   if (+process.env.XOA_PLAN === 5) {
+#     this.displayOpenSourceDisclaimer()
+#   }
+#
+
+patch_file() {
+
+        mv "$file" "$file.bak"
+        awk '
+                /\+process.env.XOA_PLAN[ \t]+=+[ \t]+5/         { start_patch=1 ; next }
+                /this\.displayOpenSourceDisclaimer/             { next }
+                start_patch == 1 && /\}/                        { start_patch=0 ; next }
+
+                1
+                ' "$file.bak" >"$file"
+        }
+
+main() {
+
+        myinit
+        systemctl stop xo-server
+        patch_file
+        cd xen-orchestra
+        yarn build
+        cd -
+        systemctl stop xo-server
+
+}
+
+export file="xen-orchestra/packages/xo-web/src/xo-app/index.js"
+main
